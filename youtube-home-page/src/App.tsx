d123cc3800a16f5a2036a8dbb1c1c888f717798b
Kariuki11 <irungukenneth433@gmail.com>
2024-06-18 02:24:19 +0300

    New project

---
+++ youtube-home-page/src/App.tsx
@@ -0,0 +1,7 @@
+import { PageHeader } from "./layouts/PageHeader";
+
+export default function App() {
+  return <div className="max-h-screen flex flex-col">
+    <PageHeader />
+  </div>
+}
---
+++ youtube-home-page/src/layouts/PageHeader.tsx
@@ -0,0 +1,16 @@
+import { Menu } from "lucide-react"
+import logo from "../assets/logo.png"
+
+export function PageHeader() {
+    return <><div className="flex gap-10 lg:gap-20 justify-between"></div>
+    <div className="flex gap-4 items-center flex-shrink-0">
+        <button>
+            <Menu />
+        </button>
+        <a href="/">
+            <img src={logo} className="h-6" />
+        </a>
+    </div>
+    <div></div>
+    <div></div></>
+}
