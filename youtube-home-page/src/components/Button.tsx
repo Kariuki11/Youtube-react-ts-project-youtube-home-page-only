6131e7ba7bec69b8cd975accd66a5137e5711b28
Kariuki11 <irungukenneth433@gmail.com>
2024-06-18 03:12:33 +0300

    New changes

--- youtube-home-page/src/components/Button.tsx
+++ youtube-home-page/src/components/Button.tsx
@@ -1,6 +1,25 @@
 import { cva } from "class-variance-authority"
-const buttonStyles = cva(["hover:bg-secondary-hover"])
+const buttonStyles = cva([ "transition-colors"], {
+    variants: {
+        variant: {
+            default: ["bg-secondary", "hover:bg-secondary-hover"],
+            ghost: ["hover:bg-gray-100"]
+        },
+        size: {
+            default: ["rounded", "p-2"],
+            icon: ["rounded-full",
+                "w-10", "h-10",
+                "flex",
+                "items-center",
+                "justify-center",
+                "p-2.5",
+            ],
+        }
+    }
+})
 
-export function Button() {
-    return <Button />
+// const classes = buttonStyles({ size: "icon" })
+
+export function Button({ variant, size }: ButtonProps) {
+    return <Button className={buttonStyles({ variant, size })} />
 }
